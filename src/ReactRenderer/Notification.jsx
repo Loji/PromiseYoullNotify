1d1a6c9f0a64771b37831adb6db5d8e59676e088
Loji <jakub.lechocki@gmail.com>
2017-10-02 23:30:02 +0200

    Adding notifications types

--- src/ReactRenderer/Notification.jsx
+++ src/ReactRenderer/Notification.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import styles from './notification.scss';
 
 export const Notification = props => (
-    <div className={styles.notificationList_notification}>
+    <div className={`${styles.notification} ${styles[props.settings.type]}`}>
         jestem notyfikacją {props.id}
         {props.settings.text}
+        {props.settings.type}
         <button onClick={props.destroy}>delete</button>
     </div>
 );
